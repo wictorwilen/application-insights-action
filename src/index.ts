e2888949242031d6744d009c045fac5c0146fdf5
Wictor Wilen <wictor@wictorwilen.se>
2020-05-23 16:14:20 +0200

    Now with tracking

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import * as core from '@actions/core';
 import github from '@actions/github';
 import { v4 } from 'uuid';
 import request from 'request';
-
+import * as ai from '@microsoft/applicationinsights-core-js';
 
 const startUrl = 'http://go.microsoft.com/fwlink/?prd=11901&pver=1.0&sbp=Application%20Insights&plcid=0x409&clcid=0x409&ar=Annotations&sar=Create%20Annotation';
 
 
 async function run(): Promise<void> {
+    const appInsights = new ai.AppInsightsCore();
+    appInsights.initialize({
+        instrumentationKey: "9d21cda9-51e5-4257-83ab-c16261b1bf4e"
+    }, []);
     try {
+
+
         core.debug(`Reading settings`)
         const applicationId = core.getInput('applicationId');
         const apiKey = core.getInput('apiKey');
@@ -22,6 +28,10 @@ async function run(): Promise<void> {
         request(startUrl, { followRedirect: false }, (error, response, body) => {
             if (error) {
                 console.log('Redirect failed');
+                appInsights.track({
+                    name: "RedirectFailed",
+                    data: error
+                });
                 core.setFailed(error);
             } else {
                 const location = response.headers['location'];
@@ -54,12 +64,23 @@ async function run(): Promise<void> {
                 request(options, (error, response, body) => {
                     if (error) {
                         console.log('Annotation failed');
+                        appInsights.track({
+                            name: "AnnotationFailed",
+                            data: error
+                        });
                         core.setFailed(error);
                     } else {
                         if (response.statusCode === 200) {
                             console.log('Annotation sent')
+                            appInsights.track({
+                                name: "AnnotationSent",
+                            });
                             core.setOutput("result", body);
                         } else {
+                            appInsights.track({
+                                name: "AnnotationFailed2",
+                                data: { status: response.statusCode, message: response.statusMessage }
+                            });
                             core.setFailed(`HTTP status: ${response.statusCode}`);
                         }
                     }
@@ -68,6 +89,10 @@ async function run(): Promise<void> {
         });
     }
     catch (error) {
+        appInsights.track({
+            name: "FatalError",
+            data: error
+        });
         console.log(error);
         core.setFailed(error.message);
     }
